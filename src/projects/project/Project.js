78dabc174d7e4c1640c9bb34ca663853c98ec1ed
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-18 22:13:43 +0300

    project in progress

--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -7,8 +7,10 @@ function Project(props) {
             <div className={style.icon}>
                 <a href={'#'}>Show</a>
             </div>
-            <h3>{props.title}</h3>
-            <div className={style.description}>{props.description}</div>
+            <div className={style.projectInfo}>
+                <h3>{props.title}</h3>
+                <div className={style.description}>{props.description}</div>
+            </div>
         </div>
     );
 }
