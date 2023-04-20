22e7f8398a4894e005702cc756f8e31c7d227c75
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-20 16:22:14 +0300

    css to scss

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './Projects.module.css';
+import style from './Projects.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
 import Project from './project/Project';
 import Title from '../common/components/title/Title';
--- src/projects/project/Project.js
+++ src/projects/project/Project.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './Project.module.css';
+import style from './Project.module.scss';
 
 function Project(props) {
     return (
