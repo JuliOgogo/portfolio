86d3e50a4bc5e9511169cd0459d07ec17cd371a6
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-21 17:44:16 +0300

    about me page

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import style from './Main.module.css';
+import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
+import myPhoto from '../assets/images/myPhoto.jpg'
 
 function Main() {
     return (
         <div className={style.main}>
-            <div className={styleContainer.container}>
+            <div className={`${styleContainer.container} ${style.mainContainer}`}>
                 <div className={style.text}>
-                    <span>Hi There</span>
-                    <h1>I am Julia</h1>
-                    <p>Frontend Developer</p>
+                    <span>Hi There!</span>
+                    <h1>I am <span>Julia Titkova</span></h1>
+                    <p>Frontend Developer based in Moscow</p>
+                    <div className={style.btnContainer}>
+                        <a href="#">download my cv</a>
+                    </div>
                 </div>
                 <div className={style.photo}>
-
+                    <img src={myPhoto} alt={'Photo'}/>
                 </div>
             </div>
         </div>
