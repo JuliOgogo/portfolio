e9e7e38a0e9d2eb5c505812d182961252777fbc8
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-23 22:49:28 +0300

    header and icons

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,6 +2,8 @@ import React from 'react';
 import style from './Main.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
 import myPhoto from '../assets/images/myPhoto.jpg'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faDownload} from '@fortawesome/free-solid-svg-icons'
 
 function Main() {
     return (
@@ -12,7 +14,7 @@ function Main() {
                     <h1>I'm <span>Julia Titkova</span></h1>
                     <p>Frontend Developer based in Moscow</p>
                     <div className={style.btnContainer}>
-                        <a href="#">download my cv</a>
+                        <a href="#"><FontAwesomeIcon icon={faDownload} className={style.icon}/>download my cv</a>
                     </div>
                 </div>
                 <div className={style.photo}>
--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import style from './Nav.module.css';
+import style from './Nav.module.scss';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHome} from '@fortawesome/free-solid-svg-icons'
+import {faList} from '@fortawesome/free-solid-svg-icons'
+import {faBriefcase} from '@fortawesome/free-solid-svg-icons'
+import {faMessage} from '@fortawesome/free-solid-svg-icons'
 
 function Nav() {
     return (
         <div className={style.nav}>
-            <a href={'#'}>Главная</a>
-            <a href={'#'}>Скиллы</a>
-            <a href={'#'}>Проекты</a>
-            <a href={'#'}>Контакты</a>
+            <a href={'#'}><FontAwesomeIcon icon={faHome} className={style.icon}/>Home</a>
+            <a href={'#'}><FontAwesomeIcon icon={faList} className={style.icon}/>Skills</a>
+            <a href={'#'}><FontAwesomeIcon icon={faBriefcase} className={style.icon}/>Projects</a>
+            <a href={'#'}><FontAwesomeIcon icon={faMessage} className={style.icon}/>Contacts</a>
         </div>
     );
 }
