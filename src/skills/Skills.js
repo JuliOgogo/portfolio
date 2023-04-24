6f4ac17515e04ea65dd2beed1964aa454da4882d
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-24 15:51:09 +0300

    skills and icons

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import style from './Skills.module.css';
 import styleContainer from '../common/styles/Container.module.css';
-import Skill from "./skill/Skill";
-import Title from "../common/components/title/Title";
+import Skill from './skill/Skill';
+import Title from '../common/components/title/Title';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faReact} from '@fortawesome/free-brands-svg-icons'
+import {faJs} from '@fortawesome/free-brands-svg-icons'
+import {faHtml5} from '@fortawesome/free-brands-svg-icons'
+import {faCss3} from '@fortawesome/free-brands-svg-icons'
+import {faSass} from '@fortawesome/free-brands-svg-icons'
+import {faGithub} from '@fortawesome/free-brands-svg-icons'
+import {faJira} from '@fortawesome/free-brands-svg-icons'
+import {faFigma} from '@fortawesome/free-brands-svg-icons'
+import {SiRedux, SiAxios, SiTypescript, SiMaterialdesign, SiStorybook} from 'react-icons/si';
 
 function Skills() {
     return (
@@ -10,9 +20,20 @@ function Skills() {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title text1={'Skills'}/>
                 <div className={style.skillsElements}>
-                    <Skill title={'HTML'} description={'Some description for HTML'}/>
-                    <Skill title={'JS'} description={'Some description for JS Some description for JSSome description for JS Some description for JSSome description for JS'}/>
-                    <Skill title={'React'} description={'Some description for React'}/>
+                    <Skill title={'React'} icon={<FontAwesomeIcon icon={faReact}/>}/>
+                    <Skill title={'Redux'} icon={<SiRedux/>}/>
+                    <Skill title={'Axios'} icon={<SiAxios/>}/>
+                    <Skill title={'JavaScript'} icon={<FontAwesomeIcon icon={faJs}/>}/>
+                    <Skill title={'TypeScript'} icon={<SiTypescript/>}/>
+                    <Skill title={'HTML'} icon={<FontAwesomeIcon icon={faHtml5}/>}/>
+                    <Skill title={'CSS'} icon={<FontAwesomeIcon icon={faCss3}/>}/>
+                    <Skill title={'SASS-SCSS'} icon={<FontAwesomeIcon icon={faSass}/>}/>
+                    <Skill title={'Material UA'} icon={<SiMaterialdesign/>}/>
+                    <Skill title={'Storybook'} icon={<SiStorybook/>}/>
+                    <Skill title={'Unit Tests'} icon={<FontAwesomeIcon icon={faReact}/>}/>
+                    <Skill title={'GIT'} icon={<FontAwesomeIcon icon={faGithub}/>}/>
+                    <Skill title={'JIRA'} icon={<FontAwesomeIcon icon={faJira}/>}/>
+                    <Skill title={'Figma'} icon={<FontAwesomeIcon icon={faFigma}/>}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -4,9 +4,10 @@ import style from './Skill.module.css';
 function Skill(props) {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
             <h3>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
+            <div className={style.icon}>
+                {props.icon}
+            </div>
         </div>
     );
 }
