b11b100a73fd3e6440b5e2560b16460df28d66c0
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-27 10:24:54 +0300

    contacts in progress

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import style from './Contacts.module.css';
+import style from './Contacts.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
+import Title from '../common/components/title/Title';
+import Button from '../common/components/button/Button';
 
 function Contacts() {
     return (
         <div className={style.contacts} id={'contacts'}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
-                <h2 className={style.title}>Contacts</h2>
+                <Title text={'Contacts.'}/>
                 <form className={style.contactsForm} action="#">
                     <input/>
                     <input/>
                     <textarea/>
+                    <Button theme={'black'} text={'Submit'} link={'#'}/>
                 </form>
-                <a className={style.contactsA} href={'#'}>Submit</a>
             </div>
         </div>
     );
