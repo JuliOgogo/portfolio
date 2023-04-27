a68499946c1f2e800a710485fea7f9c39bdac839
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-27 10:08:24 +0300

    icons in skills

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -3,8 +3,9 @@ import style from './Skills.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
 import Skill from './skill/Skill';
 import Title from '../common/components/title/Title';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faReact} from '@fortawesome/free-brands-svg-icons'
+import {HiOutlineComputerDesktop} from 'react-icons/hi2';
+import {IoCheckboxOutline, IoColorPaletteOutline} from 'react-icons/io5';
+import {MdOutlineTagFaces} from 'react-icons/md';
 
 function Skills() {
     return (
@@ -12,17 +13,17 @@ function Skills() {
             <div className={`${styleContainer.container} ${style.skillsContainer}`}>
                 <Title text={'Skills.'}/>
                 <div className={style.skillsElements}>
-                    <Skill icon={<FontAwesomeIcon icon={faReact}/>}
+                    <Skill icon={<HiOutlineComputerDesktop/>}
                            title={'Web Development'}
                            description={'JavaScript, TypeScript, React, Redux, Axios'}/>
-                    <Skill icon={<FontAwesomeIcon icon={faReact}/>}
+                    <Skill icon={<IoColorPaletteOutline/>}
                            title={'Web Design'}
                            description={'HTML, CSS, SASS-SCSS, Material UI'}/>
-                    <Skill icon={<FontAwesomeIcon icon={faReact}/>}
+                    <Skill icon={<IoCheckboxOutline/>}
                            title={'Testing'}
                            description={'Unit Tests, StoryBook, SnapShot'}/>
-                    <Skill icon={<FontAwesomeIcon icon={faReact}/>}
-                           title={'Other'}
+                    <Skill icon={<MdOutlineTagFaces/>}
+                           title={'Helpful skills'}
                            description={'Git, Jira, Figma'}/>
                 </div>
             </div>
