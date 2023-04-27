9648a2e1c48f02d4e210859fe0ce3edfc4d50fae
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-27 21:49:15 +0300

    contacts and form with formik

--- src/contacts/Contacts.tsx
+++ src/contacts/Contacts.tsx
@@ -2,19 +2,14 @@ import React from 'react';
 import style from './Contacts.module.scss';
 import styleContainer from '../common/styles/Container.module.css';
 import Title from '../common/components/title/Title';
-import Button from '../common/components/button/Button';
+import ContactsForm from './contactsForm/ContactsForm';
 
 function Contacts() {
     return (
         <div className={style.contacts} id={'contacts'}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                 <Title text={'Contacts.'}/>
-                <form className={style.contactsForm} action="#">
-                    <input/>
-                    <input/>
-                    <textarea/>
-                    <Button theme={'black'} text={'Submit'} link={'#'}/>
-                </form>
+                <ContactsForm/>
             </div>
         </div>
     );
---
+++ src/contacts/contactsForm/ContactsForm.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import style from './ContactsForm.module.scss';
+import buttonStyle from '../../common/components/button/Button.module.scss';
+import {useFormik} from 'formik';
+
+function ContactsForm() {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            message: '',
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
+    return (
+        <form onSubmit={formik.handleSubmit} className={style.contactsForm}>
+            <div className={style.nameAndEmail}>
+                <input
+                    id='name'
+                    name='name'
+                    type='text'
+                    placeholder={'Name'}
+                    onChange={formik.handleChange}
+                    value={formik.values.name}
+                />
+                <input
+                    id='email'
+                    name='email'
+                    type='email'
+                    placeholder={'Email'}
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                />
+            </div>
+            <textarea
+                id='message'
+                name='message'
+                placeholder={'Message'}
+                onChange={formik.handleChange}
+                value={formik.values.email}
+            />
+            <div> {/*дивка, чтобы на кнопку не применялось свойство stretch*/}
+                <button type='submit' className={`${buttonStyle.button} ${buttonStyle.blackButton}`}>Submit</button>
+            </div>
+        </form>
+    );
+}
+
+export default ContactsForm;
