0ec13bee382d1be16d7c424c1b08da2e69359a62
Julia Titkova <juli.ogogo@yandex.ru>
2023-04-28 17:16:10 +0300

    footer

---
+++ src/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Footer.module.scss';
+
+function Footer() {
+    return (
+        <div className={style.footer}>
+            <span className={style.text}>Copy Right 2023 © All Rights Reserved</span>
+        </div>
+    );
+}
+
+export default Footer;
