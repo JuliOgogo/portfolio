adf4c4ee9dd423621b2e5ea0daa0be18b3a7e808
Julia Titkova <juli.ogogo@yandex.ru>
2023-05-15 19:19:01 +0300

    feat: typing effect in Home block

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './Home.module.scss';
 import {Fade} from 'react-awesome-reveal';
+import ReactTypingEffect from 'react-typing-effect';
 
 function Home() {
     return (
@@ -8,7 +9,18 @@ function Home() {
             <div className={style.homeContainer}>
                 <Fade duration={2000} triggerOnce>
                     <h5>Hello!</h5>
-                    <h1>I'm Julia Titkova</h1>
+                    <h1>
+                        {`I'm` + ' '}
+                        <span>
+                            <ReactTypingEffect
+                                text={['Julia Titkova', 'Frontend Developer']}
+                                speed={100}
+                                eraseSpeed={100}
+                                eraseDelay={1000}
+                                typingDelay={1000}
+                            />
+                        </span>
+                    </h1>
                 </Fade>
             </div>
         </div>
