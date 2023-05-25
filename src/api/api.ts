a1c0e3eeccc416ef877206ddeabc9740644615e4
Julia Titkova <juli.ogogo@yandex.ru>
2023-05-25 13:08:02 +0300

    feat: sending messages to personal mail by backend on vercel

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import {FormType} from '../contacts/contactsForm/ContactsForm';
 
 export const instance = axios.create({
-    baseURL: 'http://localhost:3010'
+    baseURL: 'https://portfolio-backend-self.vercel.app'
 })
 
 export const contactsAPI = {
